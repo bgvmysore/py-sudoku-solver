d26ae791378204e872ae54cba9ee2e591edec3eb
Bhargav R <40035575+bgvmysore@users.noreply.github.com>
2021-07-13 14:27:07 +0530

    Input class, fileIO

--- run.py
+++ run.py
@@ -1,19 +1,17 @@
-from Sudoku import SudokuGrid
+from sudoku import SudokuGrid, SudokuGridInput
 
 
 def main():
     puz = SudokuGrid()
-    sudoku = '1,2,3,4,5,#,7,8,9,' \
-             '1,2,3,4,5,6,7,8,9,' \
-             '1,#,3,4,5,#,7,8,9,' \
-             '1,2,#,4,5,6,7,8,9,' \
-             '1,2,3,4,5,6,7,8,9,' \
-             '1,2,3,4,5,6,7,8,9,' \
-             '1,2,3,4,5,6,#,8,9,' \
-             '1,2,#,4,5,6,7,8,9,' \
-             '1,2,3,4,5,6,7,8,9'
+    sudoku = SudokuGridInput()
+
+    sudoku.read_txt_file('puzzle.txt')
+
+    # print(sudoku.data)
+
     puz.source_grid_from(sudoku)
     print(puz)
+    puz.save_grid_to_txt('sol.txt')
 
 
 if __name__ == '__main__':
--- sudoku.py
+++ sudoku.py
@@ -3,19 +3,19 @@ class SudokuGrid:
         self.grid = [['#' for _ in range(9)] for _ in range(9)]
 
     def __str__(self):
-        ret_var = '─'*25 + '\n'
+        ret_var = '-'*25 + '\n'
         ci, cj = 0, 0
         for i in self.grid:
             ci += 1
-            ret_var += '│ '
+            ret_var += '| '
             for j in i:
                 cj += 1
                 ret_var += str(j) + " "
                 if cj % 3 == 0:
-                    ret_var += '│ '
+                    ret_var += '| '
             ret_var += '\n'
             if ci % 3 == 0:
-                ret_var += '─'*25 + '\n'
+                ret_var += '-'*25 + '\n'
                 ci = 0
         return ret_var
 
@@ -31,5 +31,33 @@ class SudokuGrid:
     def validator(num):
         if num.isdigit() and int(num) in range(1, 10):
             return int(num)
-        else:
+        elif num == '#':
             return '#'
+        elif num == '':
+            return
+        else:
+            raise ValueError
+
+    def save_grid_to_txt(self, path_to_txt_file):
+        file = open(path_to_txt_file, "w")
+        file.write(self.__str__())
+        file.close()
+
+
+class SudokuGridInput:
+    def __init__(self):
+        self.data = None
+
+    def read_txt_file(self, path_to_txt_file, sep=' '):
+        file = open(path_to_txt_file, "r")
+        lines = file.readlines()
+        self.data = ''
+        for i in lines:
+            self.data += i
+        file.close()
+        self.data = self.data.replace('\n', ',')
+        if len(self.data.replace(',', '')) != 81:
+            raise IOError
+
+    def __str__(self):
+        return self.data
